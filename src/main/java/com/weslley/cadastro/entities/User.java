f501472dc426899624f445b299e6746a51b74b3e
weslleyFA <weslleyfaustino2013@gmail.com>
2022-08-23 21:32:38 -0300

    alteracao no nome da tabela no banco

--- src/main/java/com/weslley/cadastro/entities/User.java
+++ src/main/java/com/weslley/cadastro/entities/User.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 @Entity
+@Table(name = "Usuario")
 public class User implements Serializable {
     public static final long serialVersionUID = 1L;
     @Id
